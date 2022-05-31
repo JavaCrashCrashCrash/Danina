dc8ebb99992e18eb5dc98e0005292801fe82511e
jang6129 <jang6129@naver.com>
2022-05-31 17:22:02 +0900

    first commit

---
+++ src/main/java/com/doma/danina/DaninaApplication.java
@@ -0,0 +1,13 @@
+package com.doma.danina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaninaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaninaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doma/danina/DaninaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doma.danina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaninaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
